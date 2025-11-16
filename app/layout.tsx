cf980cfffae3170d9331058a41cf78fc6d7583bf
Piyush <piyushpanwar808@gmail.com>
2025-11-16 19:33:55 +0530

    icon updation1

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
   
   icons: {
     icon: [
-      { url: '/logo.png', sizes: '32x32', type: 'image/png' },
-      { url: '/logo.png', sizes: '192x192', type: 'image/png' },
-      { url: '/logo.png', sizes: '512x512', type: 'image/png' },
+      { url: '/logo1.png', sizes: '32x32', type: 'image/png' },
+      { url: '/logo1.png', sizes: '192x192', type: 'image/png' },
+      { url: '/logo1.png', sizes: '512x512', type: 'image/png' },
     ],
-    apple: '/logo.png',
+    apple: '/logo1git add.png',
   }
 };
 
